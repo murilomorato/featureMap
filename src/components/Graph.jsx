3d7f7f89e4dabb64729088dba8c0307382c43ec7
murilomorato <murilo@nextar.com.br>
2024-11-30 03:01:44 -0300

    refatoração do componente Graph e adição de arquivo utils para gerenciamento da câmera

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,42 +3,34 @@ import * as THREE from 'three';
 import * as d3 from 'd3';
 import Point from './atoms/Point';
 import { randomPosition } from '../utils/pointPosition';
+import { createCamera, createRenderer, handleResize } from '../utils/threeHelpers';
 
 const Graph = () => {
-    const mountRef = useRef(null);
-    const sceneRef = useRef(new THREE.Scene());
 
+    //mock dos points------
     const radius = 10;
-    const numPoints = 100; // Número de pontos a serem gerados
+    const numPoints = 100; // qtd points
     const points = Array.from({ length: numPoints }, (_, id) => ({
         id,
         name: `Point ${id}`,
         position: randomPosition(radius),
     }));
+    //-------------------
+
+    const mountRef = useRef(null);
+    const sceneRef = useRef(new THREE.Scene());
 
     useEffect(() => {
         const width = window.innerWidth;
         const height = window.innerHeight;
-        const aspect = width / height;
         const frustumSize = 10;
 
-        const camera = new THREE.OrthographicCamera(
-            frustumSize * aspect / -2,
-            frustumSize * aspect / 2,
-            frustumSize / 2,
-            frustumSize / -2,
-            1,
-            1000
-        );
-        camera.position.z = 10;
-
-        const renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setSize(width, height);
-        renderer.setClearColor(0xffffff, 1); // Define a cor de fundo para branco
+        const camera = createCamera(width, height, frustumSize);
+        const renderer = createRenderer(width, height);
         mountRef.current.appendChild(renderer.domElement);
 
-        const zoomSensitivity = 1.0; // Ajuste a sensibilidade do zoom conforme necessário
-        const moveSensitivity = 0.1; // Ajuste a sensibilidade do movimento conforme necessário
+        const zoomSensitivity = 1.0; // Ajuste da sensibilidade do zoom 
+        const moveSensitivity = 0.1; // Ajuste da sensibilidade do movimento
 
         const zoom = d3.zoom()
             .scaleExtent([0.1, 10])
@@ -62,22 +54,12 @@ const Graph = () => {
         };
         animate();
 
-        const handleResize = () => {
-            const width = window.innerWidth;
-            const height = window.innerHeight;
-            const aspect = width / height;
-            renderer.setSize(width, height);
-            camera.left = frustumSize * aspect / -2;
-            camera.right = frustumSize * aspect / 2;
-            camera.top = frustumSize / 2;
-            camera.bottom = frustumSize / -2;
-            camera.updateProjectionMatrix();
-        };
+        const onResize = () => handleResize(renderer, camera, frustumSize);
 
-        window.addEventListener('resize', handleResize);
+        window.addEventListener('resize', onResize);
 
         return () => {
-            window.removeEventListener('resize', handleResize);
+            window.removeEventListener('resize', onResize);
             mountRef.current.removeChild(renderer.domElement);
         };
     }, []);
